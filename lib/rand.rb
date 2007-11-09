f555b5e19f1c0d86768b9a24eeeba98e9489903f
Bart Teeuwisse <bart.teeuwisse@thecodemill.biz>
2007-11-08 22:51:59 -0800

    [project @ bart.teeuwisse@thecodemill.biz-20071109065159-q0rotto526b6ydnl] - Restrict rand to characters that Rails can route

--- lib/rand.rb
+++ lib/rand.rb
@@ -2,7 +2,7 @@ module StringMixin
   module Rand
     String.class_eval(%q{
 def self.rand(len)
-  return Array.new(len){(32+Kernel.rand(94)).chr}.join.chomp
+  return Array.new(len){(64+Kernel.rand(62)).chr}.join.chomp
 end})
   end
 end
