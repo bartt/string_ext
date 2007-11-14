e127dbf4880c0e5b900e37ecd43dbf558bb279c1
Bart Teeuwisse <bart.teeuwisse@thecodemill.biz>
2007-11-13 22:13:23 -0800

    [project @ bart.teeuwisse@thecodemill.biz-20071114061323-mp3cy3yna130f78h] - Added RDOC documentation - Added GNU Lesser Public license

--- Rakefile
+++ Rakefile
@@ -18,5 +18,6 @@ Rake::RDocTask.new(:rdoc) do |rdoc|
   rdoc.title    = 'StringExt'
   rdoc.options << '--line-numbers' << '--inline-source'
   rdoc.rdoc_files.include('README')
+  rdoc.rdoc_files.include('LICENSE')
   rdoc.rdoc_files.include('lib/**/*.rb')
 end
--- lib/rand.rb
+++ lib/rand.rb
@@ -1,5 +1,6 @@
-module StringMixin
-  module Rand
+module StringMixin #:nodoc:
+  # Add class method <tt>rand(len)</tt> to class String.
+  module Rand #:doc:
     String.class_eval(%q{
 def self.rand(len)
   return Array.new(len){(64+Kernel.rand(62)).chr}.join.chomp
