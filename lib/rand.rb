e4cd29a9e5517e1871bcc01175b65182b39aa434
Bart Teeuwisse <bart.teeuwisse@thecodemill.biz>
2008-02-13 14:24:56 -0800

    [project @ bart.teeuwisse@thecodemill.biz-20080213222456-i9m3tn04bulpnngo] - Documentation tweaks

--- lib/rand.rb
+++ lib/rand.rb
@@ -1,11 +1,11 @@
-module StringMixin #:nodoc:
+module StringMixin
   numbers = (48..57).to_a
   caps = (65..90).to_a
   lowers = (97..122).to_a
   @@pool = numbers + caps + lowers
   @@range = @@pool.length
 
-  # Generate a string of random characters taken from a pool of
+  # Generate a string of random characters of length +len+ taken from a pool of
   # characters that don't require URL escaping.
   def rand(len)
     return Array.new(len){(@@pool[Kernel.rand(@@range)]).chr}.join.chomp
